423671fbbda3d10ea7952e099eafaa25c9128446
Leonard Halling <leonardh@kth.se>
2017-09-07 15:13:51 +0200

    added Main

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+/**
+ * Created by Leonard on 2017-09-07.
+ */
+
+import java.util.Scanner;
+
+public class Main {
+
+    Scanner sc;
+    String[][] A;
+    String[][] B;
+    String[][] pi;
+
+
+
+    Main(Scanner sc){
+        this.sc = sc;
+        this.createMatrix();
+    }
+
+    void createMatrix(){
+        while(sc.hasNext()){
+
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
